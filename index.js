e411ec8d3dfaf475eed39a5d32473f59e1def138
cdaringe <cdieringer@mrn.org>
2015-08-04 14:19:21 -0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    development: {
+        NODE_ENV: 'development',
+        COINS_ENV: 'development'
+    },
+    staging: {
+        NODE_ENV: 'staging',
+        COINS_ENV: 'staging'
+    },
+    production: {
+        NODE_ENV: 'production',
+        COINS_ENV: 'production'
+    }
+};
