fd93660fb012d3ffd5d50e09416feab4153ee508
Dylan Wood <dwood@mrn.org>
2015-09-16 23:04:48 -0700

    add node API hostmapping vars to env

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 module.exports = {
     development: {
         NODE_ENV: 'development',
-        COINS_ENV: 'development'
+        COINS_ENV: 'development',
+        COINS_NODE_API_SSL_PORT: 8443
     },
     staging: {
         NODE_ENV: 'staging',
-        COINS_ENV: 'staging'
+        COINS_ENV: 'staging',
+        COINS_NODE_API_HOSTNAME: 'stage-coins-api.mrn.org'
     },
     production: {
         NODE_ENV: 'production',
-        COINS_ENV: 'production'
+        COINS_ENV: 'production',
+        COINS_NODE_API_HOSTNAME: 'coins-api.mrn.org'
     }
 };
